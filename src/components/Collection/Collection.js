31b7e6d6fadb7dc8260abb009833b633c19fbaf4
Mitch Olson <mitchjolson@gmail.com>
2019-08-09 12:08:20 -0500

    rendering collection as grid

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -2,6 +2,26 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import CollectionItem from '../CollectionItem/CollectionItem'
 
+// Material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+};
+
 class Collection extends Component {
 
   componentDidMount() {
@@ -16,16 +36,11 @@ class Collection extends Component {
           Collection
         </p>
       </div>
-      <table className="collectionTable">
-        <thead>
-            <tr><th>Game</th><th>Category</th><th>Players</th><th>Playtime</th><th>Rating</th><th>&nbsp;</th><th>&nbsp;</th></tr>
-        </thead>
-        <tbody>
+      <div>
           {this.props.reduxStore.userCollection.map((game, i) => {
             return (<CollectionItem key={i} game={game} history={this.props.history}/>);
           })}
-        </tbody>
-      </table>
+      </div>
       </>
     )
   }
@@ -36,4 +51,4 @@ const mapStateToProps = (reduxStore) => ({
   reduxStore
 })
 
-export default connect(mapStateToProps)(Collection);
+export default connect(mapStateToProps)(withStyles(styles)(Collection));
--- src/components/CollectionItem/CollectionItem.js
+++ src/components/CollectionItem/CollectionItem.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Categorize from '../Categorize/Categorize'
 
+// Material UI
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+    card: {
+        maxWidth: 345,
+        minWidth: 345,
+    },
+    media: {
+        height: 300,
+    },
+};
+
 class CollectionItem extends Component {
 
     handleDetails = () => {
@@ -10,15 +31,35 @@ class CollectionItem extends Component {
     }
 
     render() {
+        const { classes } = this.props;
         return (
-            <tr>
-                <td>{this.props.game.name}</td>
-                <td>{Categorize(this.props.game.category)}</td>
-                <td>{this.props.game.min_players} - {this.props.game.max_players}</td>
-                <td>{this.props.game.playtime}</td>
-                <td>{this.props.game.rating}</td>
-                <td><button onClick={this.handleDetails}>Details</button></td>
-            </tr>
+            <div className="div-grid">
+            <Card onClick={this.handleDetails} className={classes.card}>
+                <CardActionArea>
+                    <CardMedia className={classes.media}
+                        image={this.props.game.image}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            {this.props.game.name}
+                        </Typography>
+                        <Typography component="p">
+                            {this.props.game.min_players} - {this.props.game.max_players} players<br/>
+                            {this.props.game.playtime} minutes
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            </div>
+            // <tr>
+            //     <td>{this.props.game.name}</td>
+            //     <td>{Categorize(this.props.game.category)}</td>
+            //     <td>{this.props.game.min_players} - {this.props.game.max_players}</td>
+            //     <td>{this.props.game.playtime}</td>
+            //     <td>{this.props.game.rating}</td>
+            //     <td><button onClick={this.handleDetails}>Details</button></td>
+            // </tr>
         );
     }
 }
@@ -27,4 +68,4 @@ const mapStateToProps = (reduxStore) => ({
     reduxStore
 })
 
-export default connect(mapStateToProps)(CollectionItem);
+export default connect(mapStateToProps)(withStyles(styles)(CollectionItem));
