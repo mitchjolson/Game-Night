5b1498af02054ebc926396ee48ecf709835b3d45
Mitch Olson <mitchjolson@gmail.com>
2019-08-09 09:38:46 -0500

    created saga for collection game removal

--- src/redux/sagas/removeGameSaga.js
+++ src/redux/sagas/removeGameSaga.js
@@ -2,36 +2,16 @@ import { takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
 // worker Saga: will be fired on "REGISTER" actions
-function* addGame(action) {
-    console.log('in addGame, action.payload.game is', action.payload.game);
-    console.log('in addGame, userID is', action.payload.user);
+function* removeGame(action) {
     try {
-        const response = yield axios.post(`/api/games/checkgamedb`, action.payload.game);
-        console.log('checking if game exists in DB, response.data is', response.data);
-        if (response.data === undefined || response.data.length === 0) {
-            console.log('game not yet in DB, posting game now');
-            yield axios.post(`/api/games`, action.payload.game);
-        }
-        else {
-            console.log('game already exists in DB, not posting game');
-        }
-        const response2 = yield axios.post(`/api/games/checkcollection/${action.payload.user}`, action.payload.game);
-        console.log('checking to see if user is linked to game. response2.data is:', response2.data)
-        if (response2.data === undefined || response2.data.length === 0) {
-            console.log('linking game to user in user_games');
-            yield axios.post(`/api/games/link/${action.payload.user}`, action.payload.game);
-        }
-        else {
-            console.log('user is already linked to game');
-        }
+        yield axios.delete(`/api/games/${action.payload.user}`, action.payload.game_id);
     } catch (error) {
-        console.log('Error linking game:', error);
+        console.log('Error removing game from collection:', error);
     }
 }
 
-function* addGameSaga() {
-    yield takeEvery('ADD_GAME', addGame);
-    yield takeEvery('ADD_GAME_FROM_DETAILS', addGameFromDetails)
+function* removeGameSaga() {
+    yield takeEvery('REMOVE_GAME_FROM_COLLECTION', removeGame);
 }
 
 export default removeGameSaga;
