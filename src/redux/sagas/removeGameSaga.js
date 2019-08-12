ba80ed051e8e6b60c99e309b86e38e02edd3c9e5
Mitch Olson <mitchjolson@gmail.com>
2019-08-12 09:31:45 -0500

    fetching user collection after game removal

--- src/redux/sagas/removeGameSaga.js
+++ src/redux/sagas/removeGameSaga.js
@@ -1,10 +1,12 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, put } from 'redux-saga/effects';
 import axios from 'axios';
 
 // worker Saga: will be fired on "REGISTER" actions
 function* removeGame(action) {
     try {
         yield axios.delete(`/api/games/${action.payload.user}`, { data: action.payload });
+        yield put({ type: 'FETCH_USER_COLLECTION', payload: action.payload.user })
+        
     } catch (error) {
         console.log('Error removing game from collection:', error);
     }
